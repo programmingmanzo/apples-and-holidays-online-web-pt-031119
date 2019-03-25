3da5bd72de48e663f9e4ed62a4692d1f9cd6abcb
Gerardo Manzo <programmingmanzo@gmail.com>
2019-03-25 01:53:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,8 +49,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   #code here
   holiday_hash.each do |season, holiday|
-    seaons.each do |holidays, items|
-      holidays
+    season.each do |holidays, items|
       binding.pry
   end
  end
