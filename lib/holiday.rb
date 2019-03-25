7fe10cc89f739a495fe92f7f5f12e01daf5f2099
Gerardo Manzo <programmingmanzo@gmail.com>
2019-03-25 04:44:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -92,7 +92,7 @@ end
 #season = :winter 
 #holidays  = {:christmas=>["Lights", "Wreath"], :new_years=>["Party Hats"]} 
 #holidays = :christmas 
-#items = 
+#items = ["Lights", "Wreath"]
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
