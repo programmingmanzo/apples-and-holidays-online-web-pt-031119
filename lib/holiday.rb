af1c48e4a6e954d2149d22a9dc674ef8542e29dc
Gerardo Manzo <programmingmanzo@gmail.com>
2019-03-25 05:20:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -101,7 +101,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |holidays, items|
       if items.include?("BBQ")
-        bbq_holiday << items  
+        bbq_holiday << holidays   
         #binding.pry 
       end
     end
