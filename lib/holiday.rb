a24c5029e1b29dd545a85385c4f5abf78aeb0855
Gerardo Manzo <programmingmanzo@gmail.com>
2019-03-25 02:49:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,6 +54,7 @@ end
   #remember to return the updated hash
 
 def all_winter_holiday_supplies(holiday_hash)
+  
   # return an array of all of the supplies that are used in the winter season
 
 end
